f08f603ecbe6dfdfdbecbb0287a7e4e9a1f427a4
tayseeressa <128232016+tayseeressa@users.noreply.github.com>
2023-08-10 23:20:17 +0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    function player1_name  localStorage.setItem();
+    function player2_name  localStorage.setItem();
+    window.location = "game_page.html";
+}
+
